d29484a73218c22257c44651811cd5df6070540d
Leslie <lesliexin92@gmail.com>
2023-06-18 19:27:42 +0800

    feat: 错误边界处理

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,19 @@ import './App.css'
 import { useAuth } from 'context/authContext'
 import { AuthenticatedApp } from 'authenticatedApp'
 import { UnauthenticatedApp } from 'unauthenticatedApp'
+import { FullPageErrorFallback } from 'components/lib'
+import { ErrorBoundary } from 'components/ErrorBoundary'
 
 function App() {
   const { user } = useAuth()
-  return <div className='App'>{user ? <AuthenticatedApp /> : <UnauthenticatedApp />}</div>
+
+  return (
+    <div className='App'>
+      <ErrorBoundary fallbackRender={FullPageErrorFallback}>
+        {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+      </ErrorBoundary>
+    </div>
+  )
 }
 
 export default App
